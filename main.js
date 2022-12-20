fe3aea5e0654ba187e65d0447bb9bf716ae38b70
Tae Woo Kim <taewookim02@gmail.com>
2022-12-21 01:00:30 +0800

    Add buttons, grid and layout

---
+++ main.js
@@ -0,0 +1,31 @@
+// Functions to have:
+// add
+// subtract
+// multiply
+// divide
+// operate/equals(?)  *this function takes an operator and 2 numbers, then calls one of the above
+// clear
+// +/- button
+// populate display
+
+// create HTML calculator with buttons for each digit
+// create HTML for each of the functions above
+// create HTML for "Equals" key
+// a "clear" button
+
+// TODO:
+// sketch
+// frontend
+const buttons = document.querySelector(".buttons");
+
+// create buttons and insert buttons
+// for (let col = 0; col < 4; col++) {
+//   for (let row = 0; row < 5; row++) {
+//     const button = document.createElement("button");
+//     button.setAttribute("class", "button");
+//     buttons.appendChild(button);
+//   }
+// }
+
+// logic
+// deploy
