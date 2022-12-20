a6cbc64c2d61cd56c910ac44e14d5e4157df41db
Tae Woo Kim <taewookim02@gmail.com>
2022-12-21 02:48:20 +0800

    Tweak active state and remove unnecessary code

--- main.js
+++ main.js
@@ -13,19 +13,6 @@
 // create HTML for "Equals" key
 // a "clear" button
 
-// TODO:
-// sketch
-// frontend
 const buttons = document.querySelector(".buttons");
-
-// create buttons and insert buttons
-// for (let col = 0; col < 4; col++) {
-//   for (let row = 0; row < 5; row++) {
-//     const button = document.createElement("button");
-//     button.setAttribute("class", "button");
-//     buttons.appendChild(button);
-//   }
-// }
-
 // logic
 // deploy
