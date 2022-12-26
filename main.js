8090a5217dcbf752c52c6ccdf7866195544e1491
Tae Woo Kim <taewookim02@gmail.com>
2022-12-26 13:59:31 +0800

    Add calculator functionality

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 let firstNumber = "";
 let secondNumber = "";
 let operator = "";
+let result = "";
+let isResultAvailable = false;
 const numberButtons = document.querySelectorAll("[data-number]");
 const operatorButtons = document.querySelectorAll("[data-operator]");
 const displayValueContainer = document.querySelector(".display__value");
 const displayHistoryContainer = document.querySelector(".display__history");
+const equalsButton = document.querySelector(".button__equals");
+const clearButton = document.querySelector(".button__clear");
+const deleteButton = document.querySelector(".button__delete");
 
 function add(a, b) {
   return +a + +b;
@@ -19,76 +24,152 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
+  if (a === 0 || b === 0) {
+    return alert("Dividing by zero ey? :D");
+  }
   return +a / +b;
+}
+
+function modulo(a, b) {
+  return +a % +b;
 }
 
 function operate(a, b, operator) {
   a = +a;
   b = +b;
+
   switch (operator) {
     case "+":
-      return add(a, b);
-    case "-":
-      return subtract(a, b);
-    case "*":
-      return multiply(a, b);
-    case "/":
-      return divide(a, b);
+      return formatDecimals(add(a, b));
+    case "−":
+      return formatDecimals(subtract(a, b));
+    case "×":
+      return formatDecimals(multiply(a, b));
+    case "÷":
+      return formatDecimals(divide(a, b));
+    case "%":
+      return formatDecimals(modulo(a, b));
   }
 }
 
-function resetScreen() {
-  displayValueContainer.textContent = "";
+function formatDecimals(number) {
+  if (typeof number === "undefined") return;
+  if (number % 1 !== 0) {
+    return number.toFixed(2);
+  }
+  return number;
 }
-
-function populateDisplayValue(number) {
-  // whenever the number gets clicked, it updates the display__value
-  displayValueContainer.textContent += number;
-}
-
-function populateDisplayHistory(value) {
-  // whenever the equal button is clicked, it updates the display__history
-}
-
-// however we need to display during the process.
-// Which I don't know how to
-// we also need to reset firstNumber/secondNumber/operator
-// when should we reset them?
-
-// firstNumber gets filled in (update display__value)
-// operator is chosen, in this case + (put the firstNumber & operator in the display__history)
-// after operator is chosen, secondNumber gets filled in.
-// clear display__value, and get input
-// When the equal button is chosen, we use the operate(firstNumber, secondNumber, operator)
-// and the result will be returned from the operate function
-// we then shift the whole equation (e.g. 1 + 3 = 4) to display__history
-// and we set display__value as result.
-// after that, we want to use result as either firstNumber/secondNumber to further calculate more stuff.
 
 // this will populate display
 function numberButtonHandler(number) {
-  firstNumber += number;
-  displayValueContainer.textContent = firstNumber;
+  if (result) {
+    // append number to secondNumber
+    secondNumber += number;
+    updateHistory(result);
+    isResultAvailable = false;
+    displayValueContainer.textContent = secondNumber;
+  } else if (operator === "") {
+    // if there is no operator update firstNumber
+    firstNumber += number;
+    displayValueContainer.textContent = firstNumber;
+  } else if (operator) {
+    // if there is already an operator update secondNumber
+    secondNumber += number;
+    displayValueContainer.textContent = secondNumber;
+  }
 }
 
 function operatorButtonHandler(operatorButtonTextContent) {
+  if (operator) {
+    equalsButtonHandler(); // when there is already an operator, treat it like an equals button
+  }
   operator = operatorButtonTextContent;
-  updateHistory(firstNumber);
-  console.log(operatorButtonTextContent);
+  updateHistory();
 }
 
-function updateHistory(number) {
-  displayHistoryContainer.textContent = `${number} ${operator}`;
+function updateHistory() {
+  displayHistoryContainer.textContent = `${firstNumber} ${operator} ${secondNumber}`;
+}
+
+function equalsButtonHandler() {
+  // update history
+  displayHistoryContainer.textContent = `${firstNumber} ${operator} ${secondNumber} =`;
+  // get result
+  result = operate(firstNumber, secondNumber, operator);
+  if (typeof result === "undefined") {
+    result = firstNumber;
+  }
+  displayValueContainer.textContent = result;
+  // reset values using result
+  firstNumber = result || firstNumber; // if result is present, make result the firstNumber for further calculation
+  secondNumber = "";
+  operator = "";
+  isResultAvailable = true;
+}
+
+function clearAllValues() {
+  firstNumber = "";
+  operator = "";
+  secondNumber = "";
+  result = "";
+  isResultAvailable = false;
+  displayHistoryContainer.textContent = "0";
+  displayValueContainer.textContent = "0";
+}
+
+function clearCurrentValues() {
+  // clear current value
+  operator === "" ? (firstNumber = "") : (secondNumber = "");
+  displayValueContainer.textContent = "0";
+}
+
+function deleteButtonHandler() {
+  if (secondNumber && secondNumber.length > 0) {
+    secondNumber = secondNumber.substring(0, secondNumber.length - 1);
+    displayValueContainer.textContent = secondNumber;
+  } else if (firstNumber && firstNumber.length > 0) {
+    firstNumber = firstNumber.substring(0, firstNumber.length - 1);
+    displayValueContainer.textContent = firstNumber;
+  }
+}
+
+function updateClearButtonTextContent() {
+  // check if any firstNumber, secondNumber, operator have a value
+  if (displayValueContainer.textContent === "0") {
+    clearButton.textContent = "AC";
+  } else if (firstNumber || secondNumber || operator) {
+    clearButton.textContent = "C";
+  }
 }
 
 numberButtons.forEach((button) => {
   button.addEventListener("click", (e) => {
     numberButtonHandler(e.target.textContent);
+    updateClearButtonTextContent();
   });
 });
 
 operatorButtons.forEach((button) => {
   button.addEventListener("click", (e) => {
     operatorButtonHandler(e.target.textContent);
+    updateClearButtonTextContent();
   });
+});
+
+equalsButton.addEventListener("click", (e) => {
+  equalsButtonHandler();
+  updateClearButtonTextContent();
+});
+
+clearButton.addEventListener("click", (e) => {
+  if (clearButton.textContent === "C") {
+    clearCurrentValues();
+  } else {
+    clearAllValues();
+  }
+  updateClearButtonTextContent();
+});
+
+deleteButton.addEventListener("click", (e) => {
+  deleteButtonHandler();
 });
